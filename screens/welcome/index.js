7d68183f59299c5a62bf0cb718eccaec7f8d01f7
jan Z <privet@jdagroupe.com>
2023-06-18 19:51:40 +0430

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,22 +1,34 @@
-import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import { StyleSheet } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView } from "react-native";
+import { styles } from "./styles";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
+          <Image style={styles.logo} source={require("./desktop-wallpaper-blonde-girl-fitness-dumbbell-athletic-young-woman-sport-girls.jpg")} resizeMode="cover" />
+          <Text style={[styles.text, _styles.dyYPKqOX]}>{"Create User   "}</Text>
         </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
+        <Text style={_styles2.WFeDVIyv}>{"Orjin Fitness Group\n          "}</Text>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView>;
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
+
+const _styles = StyleSheet.create({
+  dyYPKqOX: {
+    color: "#000000"
+  }
+});
+
+const _styles2 = StyleSheet.create({
+  WFeDVIyv: {
+    position: "relative",
+    width: 128,
+    height: 50,
+    color: "#000000"
+  }
+});
